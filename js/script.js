885a3ad229d8481cbfe08088aa3e2526e8f07550
Alex G Mendoza <alexgm511@gmail.com>
2015-03-06 16:50:45 -0800

    Ajax project

--- js/script.js
+++ js/script.js
@@ -11,61 +11,38 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
-
-    $greeting.text('So, you want to live at ' + address + '?');
-
     // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
-
-    // load nytimes
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633'
-    $.getJSON(nytimesUrl, function(data){
-
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
-
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
-
-    }).error(function(e){
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
-    });
-
-
-
-    // load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function(){
-        $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
-
-    $.ajax({
-        url: wikiUrl,
-        dataType: "jsonp",
-        jsonp: "callback",
-        success: function( response ) {
-            var articleList = response[1];
-
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            };
-
-            clearTimeout(wikiRequestTimeout);
-        }
-    });
-
+    // YOUR CODE GOES HERE!
+	//var params = encodeURI($('#street').val() + ", " + $('#city').val());
+	var street = $('#street').val();
+	var city = $('#city').val();
+	var myLoc = street + ", " + city;	
+	var mySrc = "https://maps.googleapis.com/maps/api/streetview?size=1200x900&location="+myLoc
+	$body.append('<img class="bgimg" src="'+mySrc+'">');
+	//console.log('<img class="bgimg" src="'+mySrc+'">');
+	
+	// Change greeting to reflect new location.
+	$greeting.text("So, you want to live at "+myLoc+"?");
+	
+	// NYTimes API code
+	var myNYTKey = "cac618e7253335e989678d8acb017ca9:15:71525051";
+	var myNYTAPI = "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=";
+	var myParams = encodeURI('glocations:("'+city+'")&sort=newest');
+	//"[q=search term&fq=filter-field:(filter-term)&additional-params=values]&api-key=####"
+	$.getJSON( myNYTAPI+myParams+'&api-key'+myNYTKey, function( data ) {
+	  var items = [];
+	  $.each( data, function( key, val ) {
+		items.push( "<li id='" + key + "'>" + val + "</li>" );
+	  });
+	 
+	  $nytElem.html(items);
+	  /*$( "<ul/>", {
+		"class": "my-new-list",
+		html: items.join( "" )
+	  }).appendTo( "body" );*/
+	});
+	
     return false;
 };
 
